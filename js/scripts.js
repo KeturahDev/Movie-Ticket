a23f9e0779785b1983cab1fbd7f84cd58c1ead98
Jamison Cozart and Keturah Howard <keturah@>
2020-01-28 15:09:58 -0800

    create form in html, and link jquery and bootstrap

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// Buisness Logic------------------
+function Ticket (movie, time, age) {
+  this.movie = movie,
+  this.time = time,
+  this.age = age
+}
+
+// User Interface Logic --------------------
+var ticket = new Ticket()
+
