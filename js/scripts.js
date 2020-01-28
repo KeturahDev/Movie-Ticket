0bb4d814a5c5d0ec4188a56ce5af0b0e09ee4bb9
Jamison Cozart and Keturah Howard <keturah@>
2020-01-28 15:55:08 -0800

    Add displayTicketDetails() function to display all details of ticket and price

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,45 @@
 // Buisness Logic------------------
-function Ticket (movie, time, age) {
+function Ticket (movie, time, age, numTickets) {
   this.movie = movie,
   this.time = time,
-  this.age = age
+  this.age = age, 
+  this.numTickets = numTickets;
 }
 
-// User Interface Logic --------------------
-var ticket = new Ticket()
+Ticket.prototype.calculatePrice = function() {
+  var price = 10;
+  if(this.movie === "new") {
+    price += 2;
+  }
+  if(this.time === "matinee") {
+    price -= 2;
+  }
+  if(this.age < 12 || this.age > 55) {
+    price -= 5;
+  }
+  price *= this.numTickets;
+  return price;
+}
 
+
+
+// User Interface Logic --------------------
+
+function displayTicketDetails(ticket) {
+  var htmlString = "<h2 class='movieTitle'>" + ticket.movie + "</h2><hr><p class=ticketInfo>Age: " + ticket.age + "<br>Time: " + ticket.time + "<br>Quantity: " + ticket.numTickets + "</p><hr><h3 class='price'>Price: $" + ticket.calculatePrice() + "</h3>";
+  $("#output").html(htmlString);
+}
+
+$(document).ready(function() {
+  $('#formy').submit(function(event) {
+    event.preventDefault();
+    
+    var movie = $('select#movie').val();
+    var time = $('select#time').val();
+    var age = $('input#age').val();
+    var numTickets = $("#numTickets").val();
+    
+    var newTicket = new Ticket(movie, time, age, numTickets)
+    displayTicketDetails(newTicket);
+  })
+})
